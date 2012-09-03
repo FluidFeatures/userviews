c3eb80947ad101bb27f4848ef5c66e366724d480
Phil Whelan <phil123@gmail.com>
2012-09-03 09:18:50 -0700

    Add comment and create constant FEATURE_ICONS_DIRECTORY

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+
+  FEATURE_ICONS_DIRECTORY = File.join(::Rails.root, "public/img/feature-icons")
+
   def index
+    # Find all the images in the "feature-icons" directory, strip off
+    # the directory and extension, to just leave a list of icon names
     @feature_names =
-      Dir.entries(File.join(::Rails.root, "public/img/feature-icons")).
+      Dir.entries(File.join(FEATURE_ICONS_DIRECTORY)).
         reject { |f| ! f.end_with? ".png" }.
         map    { |f| f.split(".")[0] }.
         sort
